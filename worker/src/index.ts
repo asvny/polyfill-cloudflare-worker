dc245f40811fd8980ab157b7ab71a61b9503894c
asvny <annamalai.saro@gmail.com>
2020-01-21 12:44:12 +1100

    Add comments

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -12,9 +12,14 @@ interface PolyFillOptions {
   unknown: string
   uaString: string
   minify: boolean
-  compression: string
 }
 
+/**
+ *  The cache key should either be a request object or URL string,
+ *  so we prepend this HOST with the hash of PolyFillOptions object
+ *
+ *  Eg: https://polyfill-clouldfare-workers.io/85882a4fec517308a70185bd12766577bef3e42b
+ */
 const HOST = 'https://polyfill-clouldfare-workers.io/'
 
 addEventListener('fetch', (event: FetchEvent) => {
@@ -51,6 +56,7 @@ async function handlePolyfill(
   let url = new URL(request.url)
   // @ts-ignore
   let cache = caches.default
+  // Get query params from the URL object and store as key-value object
   let queryParams = getQueryParamsHash(url)
 
   let {
@@ -58,34 +64,52 @@ async function handlePolyfill(
     features = 'default',
     unknown = 'polyfill',
     callback,
-    compression,
   } = queryParams
+  // By default, now it supports only with minification
   const minify = url.pathname.endsWith('.min.js')
   const uaString = request.headers.get('User-Agent') || ''
 
+  // Sort the keys and also the values and have it in same shape
   let json: PolyFillOptions = {
-    excludes: excludes ? excludes.split(',') : [],
+    excludes: excludes
+      ? excludes.split(',').sort((a, b) => a.localeCompare(b))
+      : [],
     features: featuresfromQueryParam(features, queryParams.flags),
     callback: /^[\w\.]+$/.test(callback || '') ? callback : false,
     unknown,
     uaString,
     minify,
-    compression,
   }
 
+  // Default headers for serving the polyfill js file
   const headers = {
     'cache-control':
       'public, s-maxage=31536000, max-age=604800, stale-while-revalidate=604800, stale-if-error=604800', // one-month
     'content-type': 'application/javascript',
   }
+  // Get SHA-1 of json object
   let KEY = await generateCacheKey(json)
+  // Since cache API expects first argument to be either Request or URL string,
+  // so prepend a dummy host
   let cacheKey = HOST + KEY
+  // Get the cached response for given key
   let response = await cache.match(cacheKey)
 
   try {
     if (!response) {
+      /**
+       *  If there is no cached response, generate the polyfill string
+       *  for the input object, the json object params can be
+       *  polyfill-library README file.
+       */
       let polyfillString = await getPolyfillBundle(json)
 
+      /**
+       *  Construct a response object with default headers and append a
+       *  ETag header, so that for repeated requests, it can be checked
+       *  with If-None-Match header to respond immediately with a 304
+       *  Not Modified response
+       */
       response = new Response(polyfillString, {
         headers: {
           ...headers,
@@ -93,17 +117,25 @@ async function handlePolyfill(
         },
       })
 
+      // Store the response in the response without blocking execution
       event.waitUntil(cache.put(cacheKey, response.clone()))
     }
 
+    /**
+     *   Check if cached response's ETag value and current request's
+     *   If-None-Match value are equal or not, if true respond
+     *   with a 304 Not Modified response
+     */
     let ifNoneMatch = request.headers.get('if-none-match')
     let cachedEtag = response.headers.get('etag')
     if (ifNoneMatch == cachedEtag) {
       return new Response(null, { status: 304 })
     }
 
+    // Finally, send the response
     return response
   } catch (err) {
+    // If in case of error, respond the error message with error status code
     const stack = JSON.stringify(err.stack) || err
     return new Response(stack, { status: 500 })
   }
@@ -137,6 +169,7 @@ function handleDefault(request: Request): Response {
   )
 }
 
+// Sort features along with the flags (always, gated) as a JavaScript object
 function featuresfromQueryParam(
   features: string,
   flags: string,
@@ -156,10 +189,12 @@ function featuresfromQueryParam(
   }, {} as Record<string, Flag>)
 }
 
+// Generate the polyfill source
 async function getPolyfillBundle(json: object): Promise<string> {
   return polyfillLibrary.getPolyfillString(json)
 }
 
+// Make key-value pairs object from the URL object
 function getQueryParamsHash(url: URL) {
   let queryParams: Record<string, string> = {}
   // @ts-ignore
@@ -170,6 +205,7 @@ function getQueryParamsHash(url: URL) {
   return queryParams
 }
 
+// Generate hash string which is used to store in cache and also as ETag header value
 async function generateCacheKey(opts: PolyFillOptions): Promise<string> {
   let seed = {
     excludes: opts.excludes,
@@ -183,6 +219,7 @@ async function generateCacheKey(opts: PolyFillOptions): Promise<string> {
   return hashString
 }
 
+// Normalize the user agent string, we consider only the browser family, major and minor version
 function normalizeUA(uaString: string): string {
   const ua = new UAParser(uaString).getResult()
   const family = ua.browser.name || 'Unknown'
@@ -193,12 +230,14 @@ function normalizeUA(uaString: string): string {
   return `${family}#${major}#${minor}`
 }
 
-/// Utils
+// Utils
 
+// Get the unique values of an array
 function uniqueArray<T extends string, int>(array: T[]): T[] {
   return [...new Set(array)]
 }
 
+// Generate a SHA-1 hash for a given javascript object
 async function hash(object: object): Promise<string> {
   const msgBuffer = new TextEncoder().encode(JSON.stringify(object))
   const hashBuffer = await crypto.subtle.digest('SHA-1', msgBuffer)
